3e09190ad030bf720bc0483211a2de30c5485a36
Andrii Kurdiumov <kant2002@gmail.com>
2022-09-21 20:03:19 +0800

    Remove commented code

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -78,26 +78,4 @@ static async Task AssignmentsOnConnection(WebSocket webSocket)
         // Maybe we don't have to relete ticket which is assigned.
         // await client.DeleteTicketAsync(new DeleteTicketRequest() {  TicketId = ticketId });
     }
-
-    /*
-    var receiveResult = await webSocket.ReceiveAsync(
-        new ArraySegment<byte>(buffer), CancellationToken.None);
-
-    while (!receiveResult.CloseStatus.HasValue)
-    {
-        await webSocket.SendAsync(
-            new ArraySegment<byte>(buffer, 0, receiveResult.Count),
-            receiveResult.MessageType,
-            receiveResult.EndOfMessage,
-            CancellationToken.None);
-
-        receiveResult = await webSocket.ReceiveAsync(
-            new ArraySegment<byte>(buffer), CancellationToken.None);
-    }
-
-    await webSocket.CloseAsync(
-        receiveResult.CloseStatus.Value,
-        receiveResult.CloseStatusDescription,
-        CancellationToken.None);
-    */
 }
